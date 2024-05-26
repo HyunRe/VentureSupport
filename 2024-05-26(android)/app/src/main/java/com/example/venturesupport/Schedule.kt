0f40d4eaad7a206f9ff3a7c0c63ff8de3ce34290
12345khw <12345khw@naver.com>
2024-05-26 17:43:07 +0900

    map scheduler

---
+++ 2024-05-26(android)/app/src/main/java/com/example/venturesupport/Schedule.kt
@@ -0,0 +1,9 @@
+package com.example.venturesupport
+
+data class Schedule(
+    val title: String,
+    val location: String,
+    val date: String,
+    val product: String,
+    val amount: String
+)
---
+++ 2024-05-26(android)/app/src/main/java/com/example/venturesupport/ScheduleItemDecoration.kt
@@ -0,0 +1,17 @@
+package com.example.venturesupport
+
+import android.content.Context
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ScheduleItemDecoration(private val margin: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        if (parent.getChildAdapterPosition(view) != parent.adapter!!.itemCount - 1) {
+            outRect.bottom = margin
+        }
+    }
+}
