a925156839c73f779124a8e20d035d03fb7751f7
HyunRe <93036571+HyunRe@users.noreply.github.com>
2024-05-29 16:25:45 +0900

    server

---
+++ 2024-05-29 server/project-3/src/main/java/com/example/app/model/User.java
@@ -0,0 +1,109 @@
+package com.example.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 기본키
+    @Column(name = "user_id") // 회원 ID
+    private Integer userId;
+
+    @Column(name = "user_name") // 회원 이름
+    private String username;
+
+    @Column(name = "user_email") // 회원 이메일
+    private String email;
+
+    @Column(name = "user_phone_number") // 회원 전화번호
+    private String phone;
+
+    @Column(name = "latitude") // 위도
+    private Double lat;
+
+    @Column(name = "longitude") // 경도
+    private Double lng;
+
+    @Column(name = "user_password") // 회원 비밀번호
+    private String password;
+
+    @Enumerated(EnumType.STRING) // Enum 값으로 매핑
+    @Column(name = "role") // 역할
+    private UserRole role;
+
+    // Getters and Setters
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
