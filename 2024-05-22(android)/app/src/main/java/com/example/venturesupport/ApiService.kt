e5853c1f5b910b1af4f39a57db957ef195f596d5
12345khw <117167402+12345khw@users.noreply.github.com>
2024-05-29 15:28:29 +0900

    Delete 2024-05-22(android) directory

--- 2024-05-22(android)/app/src/main/java/com/example/venturesupport/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.venturesupport
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("path_to_your_php_script")
-    fun getSchedules(): Call<List<Schedule>>
-}
