25ac114cd5b1bf7087f591ef46bfcdb80feb50d4
HyunRe <93036571+HyunRe@users.noreply.github.com>
2024-05-30 10:18:28 +0900

    Update WarehouseRepository.java

--- 2024-05-29 server/project-2/src/main/java/com/example/app/repository/WarehouseRepository.java
+++ 2024-05-29 server/project-2/src/main/java/com/example/app/repository/WarehouseRepository.java
@@ -5,6 +5,9 @@ import com.example.app.model.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Integer> {
+    List<Warehouse> findByUserUserId(Integer userId);
 }
