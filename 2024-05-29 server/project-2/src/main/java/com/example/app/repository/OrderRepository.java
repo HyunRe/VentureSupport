f27adf5a3f31c86705cc271dd84c5dbca16fd29d
HyunRe <93036571+HyunRe@users.noreply.github.com>
2024-05-30 10:17:19 +0900

    Update OrderRepository.java

--- 2024-05-29 server/project-2/src/main/java/com/example/app/repository/OrderRepository.java
+++ 2024-05-29 server/project-2/src/main/java/com/example/app/repository/OrderRepository.java
@@ -2,9 +2,13 @@
 package com.example.app.repository;
 
 import com.example.app.model.Order;
+import com.example.app.model.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByUserUserId(Integer userId);
 }
