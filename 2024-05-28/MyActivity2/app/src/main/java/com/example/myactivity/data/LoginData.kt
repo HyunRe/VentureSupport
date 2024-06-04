11057f97f82dfdd4f73cd3b383ec162623e4408e
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-04 18:21:57 +0900

    Delete 2024-05-28 directory

--- 2024-05-28/MyActivity2/app/src/main/java/com/example/myactivity/data/LoginData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myactivity.data
-
-import com.example.myactivity.data.model.User
-
-data class LoginData(
-    val username: User,
-    val email: User,
-    val password: User
-)
