cb6f9d94b1374d643948c2dce0bbd9a8cf5932f6
HyunRe <93036571+HyunRe@users.noreply.github.com>
2024-06-04 18:52:35 +0900

    Delete 2024-05-30/test2 directory

--- 2024-05-30/test2/app/src/main/java/com/example/test2/Product.kt
+++
@@ -1,9 +0,0 @@
-import com.google.gson.annotations.SerializedName
-
-data class Product(
-    @SerializedName("productId") val productId: Int?,
-    @SerializedName("name") val name: String,
-    @SerializedName("description") val description: String,
-    @SerializedName("price") val price: Double,
-    @SerializedName("quantity") val quantity: Int
-)
