abc93a30f0665eb30591efcbe89864a0dfc925da
HyunRe <93036571+HyunRe@users.noreply.github.com>
2024-06-05 12:22:56 +0900

    server

---
+++ 2024-06-05(server)/project-2/src/main/java/com/example/app/repository/VehicleInventoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.app.repository;
+
+import com.example.app.model.VehicleInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VehicleInventoryRepository extends JpaRepository<VehicleInventory, Integer> {
+    List<VehicleInventory> findByUserId(Integer userId);
+    List<VehicleInventory> findByProductId(Integer productId);
+}
